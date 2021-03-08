bcaedb1ec1e2a8140e9c04786b3f7db9a7c4ea34
Paul Juneau <juneaupaul3@gmail.com>
2021-03-08 12:55:47 -0700

    Initial commit

---
+++ games/demo/game.js
@@ -0,0 +1,443 @@
+function resize() {
+    var canvas = document.getElementById('game');
+    var canvasRatio = canvas.height / canvas.width;
+    var windowRatio = window.innerHeight / window.innerWidth;
+    var width;
+    var height;
+
+    if (windowRatio < canvasRatio) {
+        height = window.innerHeight;
+        width = height / canvasRatio;
+    } else {
+        width = window.innerWidth;
+        height = width * canvasRatio;
+    }
+
+    canvas.style.width = width + 'px';
+    canvas.style.height = height + 'px';
+};
+
+window.addEventListener('resize', resize, false);
+////////////////////
+// GAME CHALLENGE //
+////////////////////
+/** 
+ * BEGIN PONG
+ */
+//TODO idea: Rename to paddleColorByKeyMap?
+/**
+ * @description color wheel spoke correlates to coresponding major key in circle of fifths.
+ * @see https://www.w3schools.com/colors/colors_wheels.asp
+ */
+let leftPaddleColorByKeyMap = new Map();
+//Acceptable note letters: C, C#, D, D#, E, F, F#, G, G#, A, A#, B"
+function loadPaddleColorMap() {
+    for(key of ACCEPTABLE_NOTE_NAMES_ARRAY) {
+        switch (key) {
+            case 'C': //12
+                leftPaddleColorByKeyMap.set(key, [255,0,0,1]);//RED
+                break; 
+            case 'G': //1 
+                leftPaddleColorByKeyMap.set(key, [255,128,0,1]);//ORANGE
+                break; 
+            case 'D': //2
+                leftPaddleColorByKeyMap.set(key, [255,255,0,1]);//YELLOW
+                break; 
+            case 'A': //3 
+                leftPaddleColorByKeyMap.set(key, [128,255,0,1]);//Chartreuse
+                break; 
+            case 'E': //4
+                leftPaddleColorByKeyMap.set(key, [0,255,0,1]);//GREEN
+                break;     
+            case 'B': //5
+                leftPaddleColorByKeyMap.set(key, [0,255,128,1]);//SPRING GREEN
+                break;
+            case 'F#': //6
+                leftPaddleColorByKeyMap.set(key, [0,255,255,1]);//CYAN
+                break;
+            case 'C#': //7 
+                leftPaddleColorByKeyMap.set(key, [0,128,255,1]);//DODGER BLUE
+                break;  
+            case 'G#': //8 Ab on Circle of fifths 
+                leftPaddleColorByKeyMap.set(key, [0,0,255,1]);//BLUE
+                break;  
+            case 'D#': //9 Eb on Circle of fifths
+                leftPaddleColorByKeyMap.set(key, [128,0,255,1]);//ELECTRIC INDIGO
+                break;  
+            case 'A#': //10 Bb on Circle of fifths
+                leftPaddleColorByKeyMap.set(key, [255,0,255,1]);//MAGENTA
+                break;  
+            case 'F': //11
+                leftPaddleColorByKeyMap.set(key, [255,0,128,1]);//DEEP PINK
+                break;  
+            default:
+                leftPaddleColorByKeyMap.set(key, [255,0,0,1]);//RED
+        }
+    }
+}
+loadPaddleColorMap();
+//currentKey should be defined in musicConductorCtrl.js which should run before game.js
+var currentAlphaValue = leftPaddleColorByKeyMap.get(currentKey)[3];
+
+
+function gameSetup() {
+    //TODO idea: add disableLiveAudio?
+    try {
+        enableLiveAudioInput();
+    } catch(err) {
+        console.log(err);
+    }
+     //sets liveAudioInputEnabled to true in pitchdetect.js
+    var confirmScaleRestriction = confirm(
+        "Would you like to set a scale to play in?\n"+
+        "It defaults to C major scale.");
+    if(confirmScaleRestriction == true) {
+        var key = prompt(
+            "Please enter the key to restrict to.\n" +
+            "Acceptable entries: C, C#, D, D#, E, F, F#, G, G#, A, A#, B"
+            ,"C");
+        currentKey = key;
+        //scaleType should be defined in musicConductorCtrl.js which should run before game.js
+        scaleType = prompt(
+            "Please enter the scale to restrict to.\n "+
+            "Acceptable entries: major, natural minor, melodic minor, harmonic minor",
+            "major"
+        );
+        var scaleShorthandName = key + '-' + scaleType;
+        var rule = new RestrictToScaleRule(scaleShorthandName);
+        musicConductor.scaleRule = rule;
+    } else {
+        //TODO idea: disable rule?
+    }
+    var fontChange = prompt(
+        "Please enter the font you would like to for your music performance diagnostics.\n"+
+        "It defaults to 10px monospace.",
+        "10px monospace"
+    );
+    fontChange = fontChange || paddlePerfomanceFont.width + " " + paddlePerfomanceFont.height; 
+    paddlePerfomanceFont.setFont(fontChange);
+    setTimeout(() => {
+        gamePaused = false;
+    }, 3000);
+    return;
+}
+
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
+const grid = 15;
+const paddleHeight = grid * 5; // 80
+const maxPaddleY = canvas.height - grid - paddleHeight;
+
+var paddleSpeed = 6;
+var ballSpeed = 5;
+
+const leftPaddle = {
+    // start in the middle of the game on the left side
+    x: grid * 2,
+    y: canvas.height / 2 - paddleHeight / 2,
+    width: grid,
+    height: paddleHeight,
+
+    // paddle velocity
+    dy: 0
+};
+const rightPaddle = {
+    // start in the middle of the game on the right side
+    x: canvas.width - grid * 3,
+    y: canvas.height / 2 - paddleHeight / 2,
+    width: grid,
+    height: paddleHeight,
+
+    // paddle velocity
+    dy: 0
+};
+const ball = {
+    // start in the middle of the game
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    width: grid,
+    height: grid,
+
+    // keep track of when need to reset the ball position
+    resetting: false,
+
+    // ball velocity (start going to the top-right corner)
+    dx: ballSpeed,
+    dy: -ballSpeed
+};
+var collisionFail = false;
+// check for collision between two objects using axis-aligned bounding box (AABB)
+// @see https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+function collides(obj1, obj2) {
+    let collisionDetected = false;
+    let obj2_rightEdgePenetrated =  obj1.x < obj2.x + obj2.width;
+    let obj2_leftEdgePenetrated = obj1.x + obj1.width > obj2.x;
+    
+    if(obj1.x < obj2.x + obj2.width &&
+        obj1.x + obj1.width > obj2.x &&
+        obj1.y < obj2.y + obj2.height &&
+        obj1.y + obj1.height > obj2.y) 
+    {
+        collisionDetected = true;
+    }
+    else {
+        //if ball left edge is to the left of left paddel's right edge
+        if(collisionFail === false && obj2_rightEdgePenetrated) {
+            collisionFail = true;
+        }
+    }
+    return collisionDetected;
+}
+
+//TODO idea: 1P vs 2P wallball vs middle-c split via gameSetup()
+var isWallBall = true;
+const wallBallDimensions = {
+    y : grid*3,
+    height : canvas.height - grid*7,
+    width : grid,
+    leftWall_x : grid * 2,
+    rightWall_x :  canvas.width - grid * 3
+};
+
+// give some time for the player to recover before launching the ball again
+function resetBall(resetWall) {
+    // give some time for the player to recover before launching the ball again
+    setTimeout(() => {
+        ball.resetting = false;
+        ball.x = canvas.width / 2;
+        ball.y = canvas.height / 2;
+        if(resetWall) {
+            leftPaddle.x = wallBallDimensions.leftWall_x;
+            leftPaddle.y = wallBallDimensions.y;
+            leftPaddle.height = wallBallDimensions.height;
+            leftPaddle.width = wallBallDimensions.width;
+            currentAlphaValue = 1;
+        } 
+    }, 400);
+}
+
+var paddlePerfomanceFont = {
+    width   : "10px",
+    height  : "monospace",
+    setFont : function(fontString) {
+        var fontComponents = fontString.split(" ");
+        this.width  = fontComponents[0];
+        this.height = fontComponents[1];
+        rightPaddlePerfomance.width  = this.width;
+        rightPaddlePerfomance.height = this.height;
+    }
+}
+
+function CanvasObject(width, height, color, x, y, type, lineHeight) {
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.x = x;
+    this.y = y;   
+    this.lineHeight = lineHeight; 
+    this.update = function() {
+        ctx = context;
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.multiLineTextUpdate = function() {
+        var multiLineText = this.text.split('\n');
+        for (var i = 0; i<multiLineText.length; i++) {
+            context.font = this.width + " " + this.height;
+            context.fillStyle = this.color;
+            context.fillText(multiLineText[i], this.x, this.y + (i*this.lineHeight) );
+        }
+    }
+}
+const midLine = canvas.width / 2 - grid / 2;
+var leftPaddleScore = new CanvasObject("30px", "Consolas", "white", midLine-100, 40, "text");
+var rightPaddleScore = new CanvasObject("30px", "Consolas", "white", midLine+100, 40, "text");
+leftPaddleScore.text = 0;
+rightPaddleScore.text = 0;
+var rightPaddlePerfomance = new CanvasObject(paddlePerfomanceFont.width, paddlePerfomanceFont.height, "white", wallBallDimensions.leftWall_x+100, 70, "text", 15);
+rightPaddlePerfomance.text = '';
+
+//TODO idea: add clearInterval(musicPerformanceTimerVar) to start stop timmer accordingly 
+//start timer to measure music performance very 100 milliseconds (1/10th second)
+var musicPerformanceTimerVar = setInterval(setMusicalPerformanceString ,100);
+
+// game loop
+function loop() {    
+    //requestAnimationFrame(loop);
+    requestAnimationFrame(loop);
+    context.clearRect(0,0,canvas.width,canvas.height);
+
+    if(isWallBall) {
+        leftPaddle.y = wallBallDimensions.y;
+        leftPaddle.height = wallBallDimensions.height;
+    }
+
+    // move paddles by their velocity
+    leftPaddle.y += leftPaddle.dy;
+    rightPaddle.y += rightPaddle.dy;
+
+    // prevent paddles from going through walls
+    //prevent from going through top of grid
+    if (leftPaddle.y < grid) {
+        leftPaddle.y = grid;
+    }//prevent from going through bottom of grid
+    else if (leftPaddle.y > maxPaddleY) {
+        leftPaddle.y = maxPaddleY;
+    }
+    //prevent from going through top of grid
+    if (rightPaddle.y < grid) {
+        rightPaddle.y = grid;
+    }//prevent from going through bottom of grid
+    else if (rightPaddle.y > maxPaddleY) {
+        rightPaddle.y = maxPaddleY;
+    }
+
+    // draw paddles
+    let leftPaddleColorKey = currentKey;
+    // convert to relative major from minor
+    if(scaleType != undefined && scaleType.search("minor") != -1) {
+        leftPaddleColorKey = convertKeyToRelativeMajorFromMinor();
+    }
+    var red = leftPaddleColorByKeyMap.get(leftPaddleColorKey)[0];
+    var green = leftPaddleColorByKeyMap.get(leftPaddleColorKey)[1];
+    var blue = leftPaddleColorByKeyMap.get(leftPaddleColorKey)[2];
+    var alpha = currentAlphaValue;
+    if(alpha > 0 ) { 
+        context.fillStyle = "rgba("+red+","+green+","+blue+","+alpha+")";
+        context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
+    } else {
+        leftPaddle.height = leftPaddle.y; 
+        leftPaddle.width = 0;
+        leftPaddle.x = 0;
+    }
+    context.fillStyle = 'white';
+    context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
+
+    // move ball by its velocity
+    if(!gamePaused) {
+        ball.x += ball.dx;
+        ball.y += ball.dy;
+    }
+
+    // prevent ball from going through walls by changing its velocity
+    //prevent from going through top of grid
+    if (ball.y < grid) {
+        ball.y = grid;
+        ball.dy *= -1;
+    }//prevent from going through bottom of grid
+    else if (ball.y + grid > canvas.height - grid) {
+        ball.y = canvas.height - grid * 2;
+        ball.dy *= -1;
+    }
+
+    // reset ball if it goes past paddle (but only if we haven't already done so)
+    if ( (ball.x < 0 || ball.x > canvas.width) && !ball.resetting) {
+        ball.resetting = true;
+        if(ball.x > canvas.width) {
+            leftPaddleScore.text += 1;
+        }
+        if(ball.x < 0 ) {
+            rightPaddleScore.text += 1;
+            gameOver();
+        }
+        resetBall(false);
+    }
+
+    // check to see if ball collides with paddle. if they do change x velocity
+    if (collides(ball, leftPaddle)) {
+        ball.dx *= -1;
+
+        // move ball next to the paddle otherwise the collision will happen again
+        // in the next frame
+        ball.x = leftPaddle.x + leftPaddle.width;
+        currentAlphaValue -= 0.1;
+    }
+    else if (collides(ball, rightPaddle)) {
+        ball.dx *= -1;
+
+        // move ball next to the paddle otherwise the collision will happen again
+        // in the next frame
+        ball.x = rightPaddle.x - ball.width;
+    }
+
+    // draw ball
+    context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+    // draw walls
+    context.fillStyle = 'lightgrey';
+    context.fillRect(0, 0, canvas.width, grid);
+    context.fillRect(0, canvas.height - grid, canvas.width, canvas.height);
+
+    // draw dotted line down the middle
+    for (let i = grid; i < canvas.height - grid; i += grid * 2) {
+        context.fillRect(midLine, i, grid, grid);
+    }
+
+    leftPaddleScore.update();
+    rightPaddleScore.update();
+    rightPaddlePerfomance.text = musicConductor.performanceString;
+    rightPaddlePerfomance.multiLineTextUpdate();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//                                   PLAYER PADDLES                                   //
+////////////////////////////////////////////////////////////////////////////////////////
+
+document.addEventListener(MidiInstrumentationEvents.MIDICHLORIANCTRLEVENT, function(e) {
+    const oneMidiChlorianCtrlrEvent = JSON.parse(e.value);
+    if (oneMidiChlorianCtrlrEvent.countIncreased ) {
+        rightPaddle.dy = -paddleSpeed;
+    } else if ( oneMidiChlorianCtrlrEvent.countDecreased ) {
+        rightPaddle.dy = paddleSpeed;
+    } else {
+        rightPaddle.dy = 0;
+    }
+    //TODO idea:score +1 if in scale; +3 if triad played; +4 if 7th played
+    if(!musicConductor.scaleRule.evaluateRule(oneMidiChlorianCtrlrEvent)) {
+        var tempHeight = rightPaddle.height/2;
+        rightPaddle.height = tempHeight;
+    }
+    setTimeout(function(){
+        rightPaddle.dy = 0; 
+        rightPaddle.height = paddleHeight;
+        }, 
+        //hardcoding beat duration for .25 second for now... assuming 4 4 time
+        //TODO idea: use beat duration instead to cause affect on object to persist while note was held down
+        250
+    );
+});
+/* END player paddle controls*/
+
+// start the game
+requestAnimationFrame(loop);
+
+function gameOver() {
+    gamePaused = true;
+    
+    var confirmRedirect = confirm(
+        "Congrats on breaking through the wall!\n"+
+        "May you breakdown all the walls that impede you! \n" +
+        "Click okay to see more details about this game and maybe some other cool things I'm working on.\n" +
+        "Click cancel to setup another game."
+    );
+    if(confirmRedirect) {
+        document.getElementById('redirectToBuyMeCoffee').click();
+    } else {
+        resetBall(true);
+        setTimeout(() => {
+            gameSetup();
+        }, 1000);
+        
+    }
+}
+/** 
+ * END PONG
+ */
+console.log('finished loading game..');
---
+++ webMidiConductor/musicConductorCtrl.js
@@ -0,0 +1,198 @@
+//DEPENDS ON ..\webMidiConductor\midiChlorianController.js having been loaded first!!!
+////////////////////////////////////////////////////////////////////////////
+//                          MUSIC THOERY RULES                            //
+////////////////////////////////////////////////////////////////////////////
+var currentKey = 'C';
+var scaleType = 'major';
+
+//assumes heptatonic (seven-note) scales that with scale degrees 1->7
+let scaleToHalfStepAlgorithm = new Map();
+scaleToHalfStepAlgorithm.set('major',2212221);
+scaleToHalfStepAlgorithm.set('natural minor',2122122);
+scaleToHalfStepAlgorithm.set('harmonic minor',2122131);
+scaleToHalfStepAlgorithm.set('melodic minor', 2122221);
+
+/**
+ * @description Constructs RestrictToScaleRule object using scaleShorthandName. 
+ * @param {String} scaleShorthandName C-major, F#-melodic minor, etc.
+ * @implements {evaluateRule()} All rules should have this method to allow dynamic rule calling via JS "duck typing". ie if it quacks, it's a duck 
+ */
+function RestrictToScaleRule(scaleShorthandName) {
+    this.ruleName = "RestrictToScaleRule";
+    //TODO idea: add this.isPass = true|false //initialize as false 
+    this.scaleShorthandName = scaleShorthandName;
+    //scaleShorthandNames: C-Major, C-Minor, etc. 
+    this.restrictToScaleArray = scaleShorthandName.split('-');
+    this.restrictedMusicKey = this.restrictToScaleArray[0];
+    //midi note numbers in ascending order that are in the given scale
+    this.restrictedMidiNoteNumbers = setRestrictedMidiNoteNumbers(this.restrictToScaleArray);
+    //midi note numbers in descending order that are in the given scale
+    this.restrictedMidiNoteNumbersDownscale = this.restrictToScaleArray[1] == 'melodic minor' ? 
+        setRestrictedMidiNoteNumbers([this.restrictToScaleArray[0],'natural minor']) : this.restrictedMidiNoteNumbers;
+    this.restrictedLetters = setRestrictedLetters(this.restrictedMidiNoteNumbers);
+    this.restrictedLettersDownscale = setRestrictedLetters(this.restrictedMidiNoteNumbersDownscale);
+    //scale degrees when progressing up the scale, ASC, versus down the scale, DESC
+    this.scaleDegreeByLetterASC = new Map();
+    this.scaleDegreeByLetterDESC = new Map();
+    this.getScaleDegreeByLetter = function(midiNoteNumbers) {
+        var scaleDegree = 1;
+        var returnMap = new Map();
+        for (let i = 0; i < midiNoteNumbers.length; i++) {
+            var oneMidiNoteNumber = midiNoteNumbers[i];
+            if(scaleDegree > 7) break;
+            returnMap.set(getNoteNameFromNumber(oneMidiNoteNumber).trim(), scaleDegree);
+            scaleDegree++;
+        }
+        return returnMap;
+    };
+    /**
+     * @description determines if given note is in restricted scale or not while considering if note is going up or down in pitch (e.g. melodic minor)
+     * @param {midiChlorianCtrlr} oneMidiChlorianCtrlrEvent is a midiChlorianCtrlr object published in an event from midiChlorianController
+     */
+    this.evaluateRule = function(oneMidiChlorianCtrlrEvent) {
+        //return true if oneMidiChlorianCtrlrEvent.midiInputPlaying is in restrictedMidiNoteNumbers
+        let midiNoteNumberToCheck = oneMidiChlorianCtrlrEvent.midiInputPlaying.note;
+        if(oneMidiChlorianCtrlrEvent.countDecreased) {
+            return this.restrictedLettersDownscale.has(getNoteNameFromNumber(midiNoteNumberToCheck).trim());
+        } 
+        if (oneMidiChlorianCtrlrEvent.countIncreased) {
+            return this.restrictedLetters.has(getNoteNameFromNumber(midiNoteNumberToCheck).trim());
+        }
+    };
+}
+
+/**
+ * @description determines restricted midi notes in provided key scale in MIDI_NOTE_NUMBER_MAPPING_INDEX 
+ * @param {String[]} restrictToScaleArray - index 0 = key (E.g. C); index 1 = scale name (E.g. Major)
+ * @returns {Integer[]} restrictedMidiNoteNumbers integer array
+ */
+function setRestrictedMidiNoteNumbers(restrictToScaleArray, highestMidiNoteNumber) {
+    //ASSUMPTIONS:
+    if(highestMidiNoteNumber === undefined) {
+       //MIDI_NOTE_NUMBER_MAPPING_INDEX[0].midiNoteNumber = 128 (G#9/Ab9) high up piano
+        highestMidiNoteNumber = MIDI_NOTE_NUMBER_MAPPING_INDEX[0].midiNoteNumber; 
+    }
+    //MIDI_NOTE_NUMBER_MAPPING_INDEX[128].midiNoteNumber = 0 (null) lowest in piano
+    var lowestMidiNoteNumber = MIDI_NOTE_NUMBER_MAPPING_INDEX[MIDI_NOTE_NUMBER_MAPPING_INDEX.length - 1].midiNoteNumber;
+    var restrictedMusicKey = restrictToScaleArray[0];
+    var foundFirstNoteOccurance = false;
+    var lowestMidiNoteNumberOccuranceInRestrictedKey;
+    var upperBoundaryIndex = MIDI_NOTE_NUMBER_MAPPING_INDEX.length - 1;
+    for( i = upperBoundaryIndex; i > 0 ; i-- ) { 
+        var currentMidiNote = MIDI_NOTE_NUMBER_MAPPING_INDEX[i];
+        var currentMidiNoteNumber = currentMidiNote.midiNoteNumber;
+        if(foundFirstNoteOccurance == false) {
+            try {
+                if(getNoteNameFromNumber(currentMidiNote.midiNoteNumber).trim() == restrictedMusicKey.toUpperCase()) {
+                    foundFirstNoteOccurance = true;
+                    lowestMidiNoteNumberOccuranceInRestrictedKey = currentMidiNoteNumber;
+                    break;
+                }
+            } catch(err) {
+                console.log('could not convert midi number to note name. midiNoteNumber: '+midiNoteNumber+' '+err.message);
+            }
+        }
+    }
+    
+    var runningMidiNoteNumber = lowestMidiNoteNumberOccuranceInRestrictedKey;
+    var scaleAlgorithm = scaleToHalfStepAlgorithm.get(restrictToScaleArray[1]);
+    var eligibleMidiNumbers = [runningMidiNoteNumber];
+    //TODO Bug? eligible midi numbers can surpass highest midi number b/c of pushing 7 notes based on scaleAlgo per running midi note number
+    while(parseInt(runningMidiNoteNumber) < parseInt(highestMidiNoteNumber)) {
+        pushNextMidiNoteNumbersInScale(scaleAlgorithm, eligibleMidiNumbers);
+        runningMidiNoteNumber = eligibleMidiNumbers[eligibleMidiNumbers.length - 1];
+    }
+    return eligibleMidiNumbers;
+}
+
+/**
+ * @description Leverages pass by reference to append midiNoteNumbers to midiNumbersArray function parameter.
+ * @param {Integer} scaleAlgorithm is a number sequence where each number represents a number of half steps to take. 
+ * @param {String[]} midiNumbersArray will get one or more midiNoteNumbers appended based on the number of half steps provided in the scaleAlgorithm.
+ * @returns void. 
+ */
+function pushNextMidiNoteNumbersInScale(scaleAlgorithm,midiNumbersArray) {
+    var lastMidiNumberInArrayAsStr = midiNumbersArray[midiNumbersArray.length - 1];
+    var lastMidiNumberInArray = parseInt(lastMidiNumberInArrayAsStr);
+    var scaleAlgoStr = scaleAlgorithm.toString();
+    for( i = 0; i < scaleAlgoStr.length; i++) {
+        var halfStepCount = parseInt(scaleAlgoStr.charAt(i));
+        lastMidiNumberInArray +=  halfStepCount;
+        midiNumbersArray.push(lastMidiNumberInArray.toString());
+    }
+}
+
+/**
+ * @description reduces restrictedMidiNoteNumbers to restrictedLettersSet based on restricted scale
+ * @param {Integer[]} restrictedMidiNoteNumbers integer array 
+ * @returns {String Set()} restrictedLettersSet  
+ */
+function setRestrictedLetters(restrictedMidiNoteNumbers) {
+    let restrictedLettersSet = new Set();
+    for(restrictedMidiNoteNumber of restrictedMidiNoteNumbers) {
+        restrictedLettersSet.add(getNoteNameFromNumber(restrictedMidiNoteNumber).trim());
+    }
+    return restrictedLettersSet; 
+}
+
+/**
+ * @description assumes calling if scaleType is minor
+   TODO idea: make stateless 
+ */
+function convertKeyToRelativeMajorFromMinor() {
+    noteLetterToFindIndexOf = currentKey;
+    let noteNameArray = ACCEPTABLE_NOTE_NAMES_ARRAY.map((x) => x);
+    //remove repeating C
+    noteNameArray.splice(12,1);
+
+    let tempNoteLetterArrayIndex = noteNameArray.findIndex(indexOfNoteLetter);
+    //convert back to relative major key
+    tempNoteLetterArrayIndex += 3; 
+    if(tempNoteLetterArrayIndex >= noteNameArray.length ) {
+        tempNoteLetterArrayIndex = tempNoteLetterArrayIndex - noteNameArray.length;
+    }
+    return noteNameArray[tempNoteLetterArrayIndex];
+}
+
+
+var musicConductor = {
+    performanceString : '',
+    scaleRule : new RestrictToScaleRule(currentKey + '-' + scaleType)
+};
+/**
+ * @returns string of musical performance: 
+ * i.e. note letter, instrumental note position, scaleDegree, chords playing  
+ */
+function setMusicalPerformanceString() {
+    musicConductor.scaleRule.scaleDegreeByLetterASC = musicConductor.scaleRule.scaleDegreeByLetterASC.size != 0 ? musicConductor.scaleRule.scaleDegreeByLetterASC 
+        : musicConductor.scaleRule.getScaleDegreeByLetter(musicConductor.scaleRule.restrictedMidiNoteNumbers);
+    musicConductor.scaleRule.scaleDegreeByLetterDESC = musicConductor.scaleRule.scaleDegreeByLetterDESC.size != 0 ? musicConductor.scaleRule.scaleDegreeByLetterDESC 
+        : musicConductor.scaleRule.getScaleDegreeByLetter(musicConductor.scaleRule.restrictedMidiNoteNumbersDownscale);
+    
+    var lettersPlaying = new Set();
+    musicConductor.performanceString = '"noteName", "scaleDegreeASC", "scaleDegreeDESC"\n';
+    for(noteObject_i of noteObjectOnByMidiNoteNumber.values()) {
+        lettersPlaying.add(noteObject_i.letter);
+        noteObject_i.scaleDegreeASC = musicConductor.scaleRule.scaleDegreeByLetterASC.get(noteObject_i.letter);
+        noteObject_i.scaleDegreeDESC = musicConductor.scaleRule.scaleDegreeByLetterDESC.get(noteObject_i.letter);
+        musicConductor.performanceString += '"'+noteObject_i.noteName+'", "'+noteObject_i.scaleDegreeASC+'", "'+noteObject_i.scaleDegreeDESC+'"\n';
+    }
+    var liveAudioInputEnabled = liveAudioInputEnabled || false;
+    if(!liveAudioInputEnabled) {
+        var chordsPlaying = [];
+        for(chordName of chordLetterSetByChordName.keys()) {
+            var oneChordLetterSet = chordLetterSetByChordName.get(chordName);
+            if(oneChordLetterSet.size <= lettersPlaying.size) {
+                var isMatch = true;
+                for (letter of lettersPlaying) {
+                    if (!oneChordLetterSet.has(letter)) {
+                        isMatch = false;
+                    }
+                }
+                if(isMatch) chordsPlaying.push(chordName);
+            }
+        }
+        if(lettersPlaying.size >0 )
+        musicConductor.performanceString += 'Chords Playing: '+ chordsPlaying.join(', ');
+    }
+} 
